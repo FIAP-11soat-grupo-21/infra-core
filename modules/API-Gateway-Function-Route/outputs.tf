0648d0ee1868df460704b32f399fc1d0a6cc0ba6
mateus lima <mateus.silva546lima@gmail.com>
2025-12-17 20:45:56 -0300

    Refactor output values in Terraform configuration to remove array indexing

--- modules/API-Gateway-Function-Route/outputs.tf
+++ modules/API-Gateway-Function-Route/outputs.tf
@@ -1,12 +1,12 @@
 output "integration_id" {
-  value = try(aws_apigatewayv2_integration.lambda[0].id, null)
+  value = try(aws_apigatewayv2_integration.lambda.id, null)
 }
 
 output "route_id" {
-  value = try(aws_apigatewayv2_route.lambda_route[0].id, null)
+  value = try(aws_apigatewayv2_route.lambda_route.id, null)
 }
 
 output "permission_statement_id" {
-  value = try(aws_lambda_permission.apigw[0].statement_id, null)
+  value = try(aws_lambda_permission.apigw.statement_id, null)
 }
 
