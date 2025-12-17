7091d4c15f1409cb5a87fd801d3de16e489f84ac
mateus lima <mateus.silva546lima@gmail.com>
2025-12-16 22:35:53 -0300

    Remove conditional counts from API Gateway integration and route resources

--- modules/API-Gateway-Function-Route/main.tf
+++ modules/API-Gateway-Function-Route/main.tf
@@ -10,7 +10,6 @@ locals {
 }
 
 resource "aws_lambda_permission" "apigw" {
-  count         = local.create_integration ? 1 : 0
   statement_id  = "AllowAPIGatewayInvoke-${var.lambda_name != "" ? var.lambda_name : substr(md5(var.lambda_arn),0,8)}"
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_arn
@@ -19,7 +18,6 @@ resource "aws_lambda_permission" "apigw" {
 }
 
 resource "aws_apigatewayv2_integration" "lambda" {
-  count                  = local.create_integration ? 1 : 0
   api_id                 = var.api_id
   integration_type       = "AWS_PROXY"
   integration_uri        = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
@@ -30,7 +28,6 @@ resource "aws_apigatewayv2_integration" "lambda" {
 }
 
 resource "aws_apigatewayv2_route" "lambda_route" {
-  count     = local.create_integration ? 1 : 0
   api_id    = var.api_id
   route_key = var.route_key
   target    = "integrations/${aws_apigatewayv2_integration.lambda[0].id}"
